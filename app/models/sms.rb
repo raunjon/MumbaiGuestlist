9a63a4bcc2ba36a1ffea994e98fbc75e179b6079
Raunak Joneja <jonejaraunak99@gmail.com>
2016-04-08 00:40:23 +0530

    changed numbers

--- app/models/sms.rb
+++ app/models/sms.rb
@@ -21,12 +21,14 @@ include Plivo
     # api.send_message(params)
     api = RestAPI.new(AUTH_ID, AUTH_TOKEN)
     entry_date_string = guestlist.entry_date.strftime('%d-%B-%Y')
+  #  number = '+919833386339'
+    number = '+919769052090'
     if guestlist.status == 1
-      message = 'You\'re entry for ' + guestlist.club.title + ' has been accepted for ' + entry_date_string + ' Please read the club policy carefully. Thank you for using MumbaiGuestlist'
+      message = 'You\'re entry for ' + guestlist.club.title + ' has been accepted for ' + entry_date_string + ' Please read the club policy carefully. For any queries, please call us at: ' + number + '. Thank you for using MumbaiGuestlist'
     elsif guestlist.status == 2
-      message = 'Sorry, we were unable to process you\'re entry for ' + guestlist.club.title + '  for ' + entry_date_string + '. Due to it being full. Please try another club. Thank you for using MumbaiGuestlist'
+      message = 'Sorry, we were unable to process you\'re entry for ' + guestlist.club.title + '  for ' + entry_date_string + '. Due to it being full. Please try another club. For any queries, please call us at: ' + number + '. Thank you for using MumbaiGuestlist'
     else
-      message = 'We have received your entry for ' + guestlist.club.title + ' for ' + entry_date_string  + ' We shall forward your details yo club and confirm it shortly.'
+      message = 'We have received your entry for ' + guestlist.club.title + ' for ' + entry_date_string  + ' We shall forward your details to club and confirm it shortly. For any queries, please call us at: ' + number + '. Thank you for using MumbaiGuestlist'
     end
 
     @message = message
